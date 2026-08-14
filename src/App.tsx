873f96cde47c7489f7dbb2faf0fb94595a9b5ae1
Adebayo Favour Ayomide <fadebayo637@gmail.com>
2026-08-14 14:49:40 +0100

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@
 
 const App = () => {
   return (
-    <div className="bg-stone-950 h-screen flex flex-col items-center justify-center gap-5">
-      <img src="/TEDX_Achievers_logo-white.png" alt="TEDxAchievers img" className='h-50' />
+    <div className="bg-stone-950 h-screen flex flex-col items-center text-center justify-center gap-5">
+      <img src="/TEDX_Achievers_logo-white.png" alt="TEDxAchievers img" className='h-20 sm:h-50' />
       <p className="text-white text-3xl font-light">COMING SOON, <span className="font-bold text-red-500">ANTICIPATE</span> !</p>
     </div>
   )
