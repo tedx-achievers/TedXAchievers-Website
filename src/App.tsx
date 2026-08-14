52f77751125f1f7e0c240d052eb1a5185d654bd1
Adebayo Favour Ayomide <fadebayo637@gmail.com>
2026-08-14 14:23:23 +0100

    Changes to Project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+const App = () => {
+  return (
+    <div className="bg-stone-950 h-screen flex flex-col items-center justify-center gap-5">
+      <img src="/TEDX_Achievers_logo-white.png" alt="TEDxAchievers img" className='h-50' />
+      <p className="text-white text-3xl font-light">COMING SOON, <span className="font-bold text-red-500">ANTICIPATE</span> !</p>
+    </div>
+  )
+}
+
+export default App
